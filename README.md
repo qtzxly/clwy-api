23e69f83edf51d5da82a57dd68ff6ed18a10ea80
qtzxly <354369716@qq.com>
2024-09-17 17:52:38 +0800

    增删改查

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Node.js 项目·······
+
+
+- Sequelize 中文文档 https://www.sequelize.cn/core-concepts/getting-started
+- 
--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -56,4 +56,81 @@ router.get('/:id', async (req, res) => {
     // res.json({id})
 })
 
+// 创建文章
+router.post('/', async (req, res) => {
+    // res.json({
+    //     data: req.body
+    // })
+    try{
+        const article = await Article.create(req.body);
+        res.status(201).json({
+            status: true,
+            message:'创建成功',
+            data: article
+        })
+    }catch(error){
+        res.status(500).json({
+            status: false,
+            message:'创建失败',
+            errors: [error.message]
+        })
+    }
+})
+
+// 删除文章
+router.delete('/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const article = await Article.findByPk(id)
+
+        if(article){
+            await article.destroy({})
+            res.json({
+                status: true,
+                message:'删除成功'
+            })
+        }else {
+            res.status(404).json({
+                status: false,
+                message:'文章不存在'
+            })
+        }
+    }catch(error){
+        res.status(500).json({
+            status: false,
+            message:'删除失败',
+            errors: [error.message]
+        })
+    }
+})
+
+// 更新文章
+router.put('/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const article = await Article.findByPk(id)
+
+        if(article){
+            await article.update(req.body)
+            res.json({
+                status: true,
+                message:'update成功',
+                data: article
+            })
+        }else {
+            res.status(404).json({
+                status: false,
+                message:'文章不存在'
+            })
+        }
+    }catch(error){
+        res.status(500).json({
+            status: false,
+            message:'update失败',
+            errors: [error.message]
+        })
+    }
+})
+
+
 module.exports = router;
