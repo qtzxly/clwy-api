f7c82cf60236894b269502002b6dac2366940f50
qtzxly <354369716@qq.com>
2024-09-24 13:42:42 +0800

    feat: 统计 chart

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const adminSettingsRouter= require('./routes/admin/settings');
 const adminUsersRouter= require('./routes/admin/users');
 const adminCoursesRouter= require('./routes/admin/courses');
 const adminChaptersRouter= require('./routes/admin/chapters');
+const adminChartsRouter= require('./routes/admin/charts');
 
 const app = express();
 
@@ -30,5 +31,6 @@ app.use('/admin/settings', adminSettingsRouter);
 app.use('/admin/users', adminUsersRouter);
 app.use('/admin/courses', adminCoursesRouter);
 app.use('/admin/chapters', adminChaptersRouter);
+app.use('/admin/charts', adminChartsRouter);
 
 module.exports = app;
---
+++ routes/admin/charts.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const {sequelize, User} = require('../../models');
+const {Op} = require('sequelize');
+const {
+  NotFoundError,
+  success,
+  failure
+} = require('../../utils/response')
+
+//  统计用户性别
+router.get('/sex', async (req,res)=>{
+  try {
+    const male = await User.count({where:{sex:0}});
+    const female = await User.count({where:{sex:1}});
+    const unknown = await User.count({where:{sex:2}});
+
+    const data = [
+      {value: male, name: '男性'},
+      {value: female, name: '女性'},
+      {value: unknown, name: '未选择'},
+    ]
+    success(res, '查询用户性别成功', {data})
+  } catch (error) {
+    failure(res,error)
+  }
+})
+
+// 统计每个月用户数量
+router.get('/user', async (req,res)=>{
+  try {
+    const [results] = await sequelize.query(
+      "SELECT DATE_FORMAT(`createdAt`, '%Y-%m') AS `month`, COUNT(*) AS `value` FROM `Users` GROUP BY `month` ORDER BY `month` ASC"
+    )
+    const data = {
+      months:[],
+      values:[]
+    }
+    results.forEach(item => {
+      data.months.push(item.month);
+      data.values.push(item.value);
+    })
+    success(res, '查询每月用户数量成功', {data})
+  } catch (error) {
+    failure(res,error)
+  }
+})
+
+module.exports = router;
